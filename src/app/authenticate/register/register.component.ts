dfc6ca590cb979a99468f0411f75b933e8313fd2
blackie832 <quandang123654@gmail.com>
2023-03-27 21:05:11 +0700

    add authenticate guard file

--- src/app/authenticate/register/register.component.ts
+++ src/app/authenticate/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { SocialUser } from "@abacritt/angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "@abacritt/angularx-social-login";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/model/User';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: SocialAuthService, private userService: AuthService, private builder: FormBuilder, private router: Router) { }
+
+
+  registerForm = this.builder.group({
+    userEmail: this.builder.control(''),
+    userPassword: this.builder.control(''),
+    userName: this.builder.control('')
+  })
 
   ngOnInit() {
   }
 
+  registerNewUser() {
+    this.userService.registerNewUser(this.registerForm.value).subscribe(response => {
+      this.router.navigate(['verify'])
+    })
+
+  }
 }
