26b4e298142ca4d817a95fee5d64df40bb06dec9
blackie832 <quandang123654@gmail.com>
2023-04-19 09:25:52 +0700

    generate pet-adoption page

--- src/app/shared/components/pet-card/pet-card.component.ts
+++ src/app/shared/components/pet-card/pet-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pet-card',
   templateUrl: './pet-card.component.html',
-  styleUrls: ['./pet-card.component.css']
+  styleUrls: ['./pet-card.component.less']
 })
 export class PetCardComponent implements OnInit {
 
