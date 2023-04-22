24ee5f7add40dcbce73d498fb41d470588dfd2ff
blackie832 <quandang123654@gmail.com>
2023-04-22 09:46:48 +0700

    display pet in pet page

--- src/app/shared/components/pet-card/pet-card.component.ts
+++ src/app/shared/components/pet-card/pet-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pet-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pet-card.component.less']
 })
 export class PetCardComponent implements OnInit {
+  @Input() pet: any;
 
   constructor() { }
 
