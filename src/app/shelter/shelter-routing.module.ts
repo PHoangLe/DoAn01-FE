ba7a36e557b5c4bd950f86a092d31c410056f7eb
blackie832 <quandang123654@gmail.com>
2023-04-28 17:39:58 +0700

    add pet dialog

--- src/app/shelter/shelter-routing.module.ts
+++ src/app/shelter/shelter-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { PetAdoptionComponent } from './pet-adoption/pet-adoption.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent
   },
+  {
+    path: 'adopt',
+    component: PetAdoptionComponent
+  }
 
 ];
 
--- src/app/shelter/shelter.module.ts
+++ src/app/shelter/shelter.module.ts
@@ -7,12 +7,28 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
+import { PaginatorModule } from 'primeng/paginator';
+import { DataViewModule } from 'primeng/dataview';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { PetAdoptionComponent } from './pet-adoption/pet-adoption.component';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { TableModule } from 'primeng/table';
+import { PetAdoptService } from '../services/pet-adopt.service';
+import { AddPetComponent } from './pet-adoption/add-pet/add-pet.component';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
     LandingComponent,
+    PetAdoptionComponent,
+    AddPetComponent
+
   ],
   imports: [
     CommonModule,
@@ -21,8 +37,19 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
-    AngularFireStorageModule
-
+    AngularFireStorageModule,
+    FileUploadModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    CheckboxModule,
+    DividerModule,
+    PaginatorModule,
+    DataViewModule,
+    RadioButtonModule,
+    BreadcrumbModule,
+    DynamicDialogModule,
+    TableModule,
+    InputTextareaModule
   ]
 })
 export class ShelterModule { }
