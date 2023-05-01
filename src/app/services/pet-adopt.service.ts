b3d2ff83be7f6873a1c05454795cdfd6744401ca
blackie832 <quandang123654@gmail.com>
2023-05-01 22:20:29 +0700

    add default pet image

--- src/app/services/pet-adopt.service.ts
+++ src/app/services/pet-adopt.service.ts
@@ -22,10 +22,25 @@ export class PetAdoptService {
 
 
   async addPet(petData: any, avatarUrl: string, otherImg: string[]): Promise<any> {
-    const headers = this.getHttpHeader();
+    let headers = this.getHttpHeader();
     const shelterID = await this.shelterService.getShelterByUserID();
+    console.log("shelterID ", shelterID)
+    console.log("animalName ", petData.petName)
+    console.log("animalAge ", petData.petAge)
+    console.log("animalGender ", petData.petGender.id)
+    console.log("animalWeight ", petData.petWeight)
+    console.log("animalBreed ", petData.petBreed)
+    console.log("animalSpecie ", petData.petSpecie.value)
+    console.log("animalColor ", petData.petColor)
+    console.log("animalImg ", avatarUrl)
+    console.log("animalStatus ", petData.petDetails)
+    console.log("vaccinated ", petData.vaccinated ? true : false)
+    console.log("deWormed ", petData.deWorm ? true : false)
+    console.log("sterilized ", petData.sterilized ? true : false)
+    console.log("friendly ", petData.friendly ? true : false)
+    console.log("othersImg ", otherImg)
     try {
-      let response = await this.http.post(this.baseUrl + '/addAnimal', {
+      const response = await this.http.post(this.baseUrl + '/addAnimal', {
         "shelterID": shelterID,
         "animalName": petData.petName,
         "animalAge": petData.petAge,
@@ -36,12 +51,12 @@ export class PetAdoptService {
         "animalColor": petData.petColor,
         "animalImg": avatarUrl,
         "animalStatus": petData.petDetails,
-        "vaccinated": petData.vaccinated,
-        "deWormed": petData.deWorm,
-        "sterilized": petData.sterilized,
-        "friendly": petData.friendly,
+        "vaccinated": petData.vaccinated ? true : false,
+        "deWormed": petData.deWorm ? true : false,
+        "sterilized": petData.sterilized ? true : false,
+        "friendly": petData.friendly ? true : false,
         "othersImg": otherImg
-      }, { headers });
+      }, { headers }).toPromise();
       return response;
     }
     catch (error) {
@@ -64,7 +79,7 @@ export class PetAdoptService {
         item.animalBreed,
         item.animalSpecie,
         item.animalColor,
-        item.animalImg,
+        item.animalImg == "" ? "https://firebasestorage.googleapis.com/v0/b/advance-totem-350103.appspot.com/o/Avatar%2Fava-default_pet_pfp.png?alt=media&token=3fcf7cb9-a92b-402e-bc2c-08d632d62ae0" : item.animalImg,
         item.animalStatus,
         item.vaccinated,
         item.deWormed,
