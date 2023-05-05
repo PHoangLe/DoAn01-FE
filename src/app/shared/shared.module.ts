24f5c1380e019014939b3e79a7f2698a0f521dac
blackie832 <quandang123654@gmail.com>
2023-05-05 08:00:41 +0700

    add footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,23 +3,31 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { MenuModule } from 'primeng/menu';
+import { DividerModule } from 'primeng/divider';
 import { AvatarModule } from 'primeng/avatar';
 import { PetCardComponent } from './components/pet-card/pet-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { WidgetComponent } from './widget/widget.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     MenuModule,
-    AvatarModule
+    AvatarModule,
+    DividerModule
   ],
   declarations: [
     HeaderComponent,
-    PetCardComponent
+    PetCardComponent,
+    FooterComponent,
+    WidgetComponent
   ],
   exports: [
     HeaderComponent,
-    PetCardComponent
+    PetCardComponent,
+    FooterComponent,
+    WidgetComponent
 
   ]
 })
